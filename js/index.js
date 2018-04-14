d301a8c34b2a339991b0313158a7134f3efefb84
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-04-14 19:11:53 -0400

    more progress

--- js/index.js
+++ js/index.js
@@ -2,8 +2,15 @@
     "user strict";
 
     var lockCourses = true;
-    var lockA08 = $(".csca08").hasClass("disabled");
-    var lockA48 = $(".csca48").hasClass("disabled");
+    var lockA08 = true;
+    var lockA48 = true;
+    var lockResume = true;
+
+    parseURL();
+
+    setLayoutPref();
+
+    window.onresize = function() {setLayoutPref()};
 
     $(".about-item").click(function() {
         enableItem(".about-item");
@@ -13,6 +20,13 @@
     $(".projects-item").click(function() {
         enableItem(".projects-item");
         enableSection("#projects");
+    });
+
+    $(".resume-item").click(function() {
+        if (!lockResume) {
+            enableItem(".resume-item");
+            enableSection("#resume");
+        }
     });
 
     $(".csca08").click(function() {
@@ -29,6 +43,10 @@
         }
     });
 
+    $(".popup").popup({ boundary: ".segment" });
+
+    $(".toggle-sidebar").click(function(){$(".custom-sidebar").sidebar("toggle");});
+
     function enableItem(item) {
         $(".item").removeClass("active");
         $(".item").removeClass("custom-active");
@@ -39,10 +57,60 @@
         $("#about").removeClass("shown");
         $("#projects").removeClass("shown");
         $(id).addClass("shown");
+        if ($(".custom-sidebar").hasClass("sidebar")) $(".custom-sidebar").sidebar("toggle");
     }
 
-    $(".popup").popup({
-      boundary: ".segment"
-    });
+    function setLayoutPref() {
+        var width = $(window).width();
+        if (width <= 800) {
+            $(".custom-sidebar").addClass("sidebar");
+        } 
+        else {
+            $(".custom-sidebar").remove("sidebar");
+        }
+    }
+
+    function parseURL() {
+        var contents = window.location.href.split("?");
+        if (contents.length == 2) {
+            if (contents[1] === "about") window.location = contents[0];
+            else if (contents[1] == "projects") {
+                enableItem(".projects-item");
+                enableSection("#projects");
+            }
+            else if (contents[1] == "resume") {
+                enableItem(".resume-item");
+                enableSection("#resume");
+            }
+            else {
+                var moreConts = contents[1].split("=");
+                if (moreConts.length == 2 && moreConts[0] == "course") {
+                    if(moreConts[1] === "csca08") {
+                        if (!lockCourses && !lockA08) {
+                            enableItem(".courses-item");
+                            $(".csca08").addClass("custom-active");
+                        }
+                        else {
+                            window.location = contents[0];
+                        }
+                    }
+                    else if (moreConts[1] === "csca48") {
+                        if (!lockCourses && !lockA48) {
+                            enableItem(".courses-item");
+                            $(".csca48").addClass("custom-active");
+                        } else {
+                            window.location = contents[0];
+                        }
+                    }
+                }
+                else {
+                    window.location = contents[0] + "404.html";
+                }
+            }
+        }
+        else if (contents.length > 2) {
+            window.location = contents[0]+"404.html";
+        }
+    }
 
 }());
