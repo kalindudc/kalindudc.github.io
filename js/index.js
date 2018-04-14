d69092aff4e7d03e0568126810c29646c7e18ca6
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-04-13 21:27:15 -0400

    some more progress, added the about page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 (function(){
     "user strict";
 
+    var lockCourses = true;
+    var lockA08 = $(".csca08").hasClass("disabled");
+    var lockA48 = $(".csca48").hasClass("disabled");
+
     $(".about-item").click(function() {
       enableItem(".about-item");
     });
@@ -10,16 +14,27 @@
     });
 
     $(".csca08").click(function() {
-        enableItem(".courses-item");
+        if (!lockCourses && !lockA08) {
+            enableItem(".courses-item");
+            $(".csca08").addClass("custom-active");
+        }
     });
 
     $(".csca48").click(function() {
-        enableItem(".courses-item");
+        if (!lockCourses && !lockA48) {
+            enableItem(".courses-item");
+            $(".csca48").addClass("custom-active");
+        }
     });
 
     function enableItem(item) {
         $(".item").removeClass("active");
+        $(".item").removeClass("custom-active");
         $(item).addClass("active");
     }
+
+    $(".popup").popup({
+      boundary: ".segment"
+    });
 
 }());
