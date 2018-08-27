f91f5f8d78d5143012ae9b8a960d80bc8db60d1d
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-08-27 18:22:14 -0400

    finalized css for the website

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 (function(){
 
-  "use strict";
-
-  var curSec = 0;
-  var secs = ["#about", "#featured", "#projects", "#resume", "#contact"];
-  var secsNav = ["#about-item", "#featured-item", "#projects-item", "#resume-item", "#contact-item"];
   
-  $(".next-button").click(function() {
-    curSec = (curSec + 1) % secs.length;
-    selectNext();
-  });
-
-  $(".navbar-item").click(function() {
-    curSec = $(this).attr("data-index");
-    selectNext();
-  });
-
-  function selectNext() {
-    $(".navbar-item").removeClass("active-nav");
-    $(secsNav[curSec]).addClass("active-nav");
-    $(".content-item").removeClass("active-content");
-    $(secs[curSec]).addClass("active-content");
-        
-  }
-
-
 
 }());
