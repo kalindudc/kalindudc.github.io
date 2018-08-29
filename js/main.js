99c333308edc05f0b4896146078c61f64d75f052
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-08-29 13:49:24 -0400

    finalized redesign

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 (function(){
 
-  
+  ScrollReveal().reveal("#home .header");
+  ScrollReveal().reveal("#home .title", {delay: 200});
+  ScrollReveal().reveal("#home .titleOther", {delay: 500});
+  ScrollReveal().reveal(".load-hidden");
 
 }());
