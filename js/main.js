e25ea34ef2071001af793414ff0289864d2743dd
Kalindu De Costa <kalindu@kalindus-mbp.war.can.ibm.com>
2019-06-10 16:51:09 -0400

    adding delay to the scroll revela to make it seem more dynamic and cool XD

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 (function(){
 
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   ScrollReveal().reveal("div", {distance: "20px"});
+  $(".project-item").each(function(){
+    ScrollReveal().reveal(this, {delay: getRandomInt(400)});
+  });
+  
 }());
